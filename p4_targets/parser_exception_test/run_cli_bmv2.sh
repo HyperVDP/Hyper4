0c3868feab1f7680b4cbc9930898b982be679107
David Hancock <dhancock@cs.utah.edu>
2016-04-08 11:12:22 -0600

    Upgrade scripts for running switch, CLI in all p4 targets.
    Ability to use either bleeding-edge or p4factory versions of
    behavioral model via scripts now extended to all p4 targets.

--- p4_targets/parser_exception_test/run_cli_bmv2.sh
+++ p4_targets/parser_exception_test/run_cli_bmv2.sh
@@ -8,4 +8,22 @@ PROJ=${PWD##*/}
 
 CLI_PATH=$BMV2_PATH/targets/simple_switch/sswitch_CLI
 
-$CLI_PATH $PROJ.json $1
+#JSON argument
+JS=
+
+#Thrift port argument
+TP=
+
+ARG1=$1
+
+if [ "$#" -gt "0" ]; then
+  if [[ $ARG1 == p4f ]]; then
+    BMV2_PATH=$THIS_DIR/../p4factory/submodules/bm
+    CLI_PATH=$BMV2_PATH/targets/simple_switch/runtime_CLI
+    JS="--json"
+    TP="--thrift-port"
+    ARG1=$2
+  fi
+fi
+
+$CLI_PATH $JS $PROJ.json $TP $ARG1
