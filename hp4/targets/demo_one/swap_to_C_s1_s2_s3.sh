7c13a70db64309bd9e93d3ed2e4e33f2d54b776d
David Hancock <dhancock@cs.utah.edu>
2016-05-22 23:33:40 -0600

    Fix a few bugs, bugs remain.

--- hp4/targets/demo_one/swap_to_C_s1_s2_s3.sh
+++ hp4/targets/demo_one/swap_to_C_s1_s2_s3.sh
@@ -6,4 +6,6 @@ source $THIS_DIR/../../../env.sh
 
 CLI_PATH=$BMV2_PATH/targets/simple_switch/sswitch_CLI
 
+$CLI_PATH ../../hp4.json 22222 < ./C_s1.txt
 $CLI_PATH ../../hp4.json 22223 < ./swap_to_C.txt
+$CLI_PATH ../../hp4.json 22224 < ./C_s3.txt
