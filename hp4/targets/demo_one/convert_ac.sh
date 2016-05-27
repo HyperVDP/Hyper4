73508ff77310c8391bf5b05cbea17d661a752687
David Hancock <dhancock@cs.utah.edu>
2016-05-27 09:40:08 -0600

    Minor update to convert_ac.sh; add optional "--test" argument.

--- hp4/targets/demo_one/convert_ac.sh
+++ hp4/targets/demo_one/convert_ac.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_A_s1_s3.txt --output A_s1.txt --progID 1
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_A_s1_s3.txt --output A_s3.txt --progID 1
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_A_s2.txt --output A_s2.txt --progID 1
+TEST_SUFFIX=
 
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_B_s1.txt --output B_s1.txt --progID 2
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_B_s2.txt --output B_s2.txt --progID 2
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_B_s3.txt --output B_s3.txt --progID 2
+if [ $# -eq 1 ]
+  then
+    if [[ $1 == "--test" ]]; then
+      TEST_SUFFIX="_test"
+    fi
+fi
 
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_C_s2_1.txt --output C_s2_1.txt --progID 3
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_C_s2_2.txt --output C_s2_2.txt --progID 4
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_C_s2_3.txt --output C_s2_3.txt --progID 5
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_A_s1_s3.txt --output A_s1$TEST_SUFFIX.txt --progID 1
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_A_s1_s3.txt --output A_s3$TEST_SUFFIX.txt --progID 1
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_A_s2.txt --output A_s2$TEST_SUFFIX.txt --progID 1
+
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_B_s1.txt --output B_s1$TEST_SUFFIX.txt --progID 2
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_B_s2.txt --output B_s2$TEST_SUFFIX.txt --progID 2
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_B_s3.txt --output B_s3$TEST_SUFFIX.txt --progID 2
+
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_C_s2_1.txt --output C_s2_1$TEST_SUFFIX.txt --progID 3
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_C_s2_2.txt --output C_s2_2$TEST_SUFFIX.txt --progID 4
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d1_C_s2_3.txt --output C_s2_3$TEST_SUFFIX.txt --progID 5
