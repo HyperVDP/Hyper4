7f9f3e0a755c3425aed206da73dedf339ec74815
David Hancock <dhancock@cs.utah.edu>
2016-06-03 12:12:59 -0600

    Revise d2 to work with latest HP4 virt networking.  Test success!

--- hp4/targets/demo_two/convert_ac.sh
+++ hp4/targets/demo_two/convert_ac.sh
@@ -11,6 +11,6 @@ fi
 
 ~/hp4-src/tools/ac_to_c.py --input commands_annotated_d2_A.txt --output A$TEST_SUFFIX.txt --progID 1
 
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d2_B_1.txt --output B_1$TEST_SUFFIX.txt --progID 2
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d2_B_1.txt --output B_1$TEST_SUFFIX.txt --progID 2 --virt_ports 65 66 67 68
 
-~/hp4-src/tools/ac_to_c.py --input commands_annotated_d2_B_2.txt --output B_2$TEST_SUFFIX.txt --progID 3
+~/hp4-src/tools/ac_to_c.py --input commands_annotated_d2_B_2.txt --output B_2$TEST_SUFFIX.txt --progID 3 --virt_ports 69 70 71 72
